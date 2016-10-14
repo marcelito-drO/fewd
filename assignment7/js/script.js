e216ee0e20cd8e6bfc2730ab940c06916590742c
marcelito-drO <marceloderada@gmail.com>
2016-10-13 20:26:34 -0700

    Assignment 7
    Finished Movie Search assignment

---
+++ assignment7/js/script.js
@@ -0,0 +1,23 @@
+var movieClick = document.querySelectorAll("img");
+
+for (var i = 0; i < movieClick.length; i++) {
+  movieClick[i].addEventListener("click",getMovie);
+}
+
+function getMovie(event) {
+  var clickedMovie = event.target;
+  var imdbID = clickedMovie.id;
+  var url = "http://omdbapi.com/?i=" + imdbID;
+
+  $.get(url, displayResults);
+}
+
+function displayResults(results) {
+
+document.querySelector("#movie-title").textContent = results.Title;
+document.querySelector("#movie-year").textContent = results.Year;
+document.querySelector("#movie-rating").textContent = results.Rated;
+document.querySelector("#movie-runtime").textContent = results.Runtime;
+document.querySelector("#movie-plot").textContent = results.Plot;
+
+}
