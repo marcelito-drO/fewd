0afccbdbd40072594ab34df9da746e1822e302ba
marcelito-drO <marceloderada@gmail.com>
2016-10-19 22:35:05 -0700

    Project Finale
    HTML and CSS built 40%. Need to start working on the JS

---
+++ project/js/script.js
@@ -0,0 +1,20 @@
+
+
+/* var button = document.querySelector(".button");
+button.addEventListener("click",showMeHomework);
+
+function showMeHomework(event) {
+  event.preventDefault();
+  var newUl = document.createElement("ul");
+  var newLi = document.createElement("li");
+
+  document.querySelector("body").appendChild(newUl);
+  newUl.appendChild(newLi);
+
+  newLi.textContent = document.querySelector("input").value;
+  console.log(newLi.textContent);
+  console.log(newLi.textContent.split(","));
+} */
+
+// The following code allows items to be saved (from previous homework)
+// Need to correct code so it saves in the right spot!
